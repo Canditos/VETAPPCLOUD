9f1910b3b80b9da7ed2ad759ae3bc6a6120e21ba
Marco Candido <marco.candido@gmail.com>
2026-05-18 15:58:42 +0100

    fix: make dotenv optional in seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,9 @@
 const { PrismaClient } = require("@prisma/client");
-require("dotenv").config();
+try {
+  require("dotenv").config();
+} catch (e) {
+  // Ignorar erro em produção
+}
 const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient({});
